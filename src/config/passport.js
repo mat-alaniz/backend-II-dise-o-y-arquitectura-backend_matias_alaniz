19b3e602f3ecec26fd03d798fa691ee8dccedc38
mat-alaniz <matias.cai.alaniz@gmail.com>
2025-08-26 21:16:19 -0300

    se agrego estrategias register y login con passport-local

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,6 +1,9 @@
 import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import { Strategy as LocalStrategy } from 'passport-local';
 import User from '../models/user.model.js';
+import Cart from '../models/cartModel.js';
+import bcrypt from 'bcrypt';
 
 const cookieExtractor = req => req.cookies?.jwt || null;
 
@@ -18,5 +21,53 @@ passport.use(new JwtStrategy(opts, async (payload, done) => {
         return done(error, false);
     }
 }));
+// estrategia para registro
+passport.use("register", new LocalStrategy(
+    { usernameField: 'email', passReqToCallback: true, session: false },
+    async (req, email, password, done) => {
+        try {
+            const existingUser = await User.findOne({ email });
+            if (existingUser) return done(null, false, { message: 'El usuario ya existe' });
+
+            const { first_name, last_name, age } = req.body;
+
+            let cart = await cart.create({ products: [] });
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const newUser = new User({
+                first_name,
+                last_name,
+                email,
+                age,
+                password: hashedPassword,
+                role: email === process.env.ADMIN_EMAIL ? 'admin' : 'user',
+                cart: cart._id
+            });
+
+            
+            return done(null, newUser);
+        } catch (error) {
+            return done(error);
+        }
+    }
+));
+
+//estrategia para login
+passport.use("login", new LocalStrategy(
+    { usernameField: 'email', session: false },
+    async (req, email, password, done) => {
+        try {
+            const user = await User.findOne({ email }).populate('cart');
+            if (!user) return done(null, false, { message: 'Usuario no encontrado' });
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) return done(null, false, { message: 'Contraseña incorrecta' });
+
+            return done(null, user);
+        } catch (error) {
+            return done(error);
+        }
+    }
+));
 
 export default passport;
